8984f2f25ac57c215a04c578997a06e2297e8f10
KMKR0910 <kasunrashmika5@gmail.com>
2026-03-02 17:59:33 +0530

    add doubly linkedlist , addFirst ,addlast methods

---
+++ LinkedList/DoublyLSExample1.java
@@ -0,0 +1,63 @@
+package LinkedList;
+
+public class DoublyLSExample1 {
+
+static Node head;
+static class Node{
+  
+   int data;
+   Node next;
+   Node prev=null;
+
+
+   Node (int d){
+      data=d;
+      next=null;
+   }
+   Node(int d,Node n,Node p){
+      data=d;
+      next=n;
+      prev=p;
+   }
+
+   public static void main(String[]args){
+      head =new Node(1);
+      Node second= new Node(2);
+      Node third = new Node(3);
+      head.next=second;
+      second.prev=head;
+      second.next=third;
+      third.prev=second;
+
+      display(head);
+      
+
+   }
+   // if you want only forward direction forward
+   //static void display(Node currNode){
+   //    while(currNode!=null){
+   //       System.out.println("Value"+currNode.data);
+   //       currNode=currNode.next;
+   //    }
+
+   // }
+
+   static void display(Node currNode){
+      Node tail= null;  //tail store value of  last node.because when end this loop currNode become null .  
+      while(currNode!=null){//after become null we cant do backward direction value print
+         System.out.println("Value"+currNode.data);
+         tail=currNode;
+         currNode=currNode.next;
+      }
+      currNode=tail;
+      while(currNode!=null){
+         System.out.println("Value"+currNode.data);
+         currNode=currNode.prev;
+      }
+   }
+
+
+}
+
+   
+}
---
+++ LinkedList/DoublyLSExample2.java
@@ -0,0 +1,72 @@
+package LinkedList;
+
+public class DoublyLSExample2 {
+
+    static Node head;
+
+    static class Node{
+      Node next;
+      Node prev;
+      int data;
+      Node(int d){
+         data=d;
+         next=null;
+      }
+      Node (int d,Node p, Node n){
+         data=d;
+         next=n;
+         prev=p;
+      }
+    }
+    public static void main(String[]args){
+      head=new Node(1);
+      Node second=new Node(2);
+      Node third= new Node(3);
+
+      head.next=second;
+      second.prev=head;
+      second.next=third;
+      third.prev=second;
+    }
+
+    static void display(Node currNode){
+      Node tail= null;  //tail store value of  last node.because when end this loop currNode become null .  
+      while(currNode!=null){//after become null we cant do backward direction value print
+         System.out.println("Value"+currNode.data);
+         tail=currNode;
+         currNode=currNode.next;
+      }
+      currNode=tail;
+      while(currNode!=null){
+         System.out.println("Value"+currNode.data);
+         currNode=currNode.prev;
+      }
+   }
+
+
+   static void addFirst(int item){
+      Node tmp= head;
+      head= new Node(item,null,head);
+      tmp.prev=head;
+
+   }
+
+   static void addLast(int item){
+      if (head==null){
+         addFirst(item);
+         
+      }
+      else{
+      Node tmp=head;
+      while(tmp.next!=null){
+         tmp=tmp.next;
+      }
+      if (tmp.next==null){
+         Node newNode=new Node(item,tmp,null); /// or simply tmp.next=new Node(item,tmp,null); /// or simply tmp.next=newNode;
+;
+         tmp.next=newNode;
+      }}
+   }
+
+   
+}
