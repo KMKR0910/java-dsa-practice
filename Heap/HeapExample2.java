6ce4d9d430878fc03da7452d875de6c582deebfc
KMKR0910 <kasunrashmika5@gmail.com>
2026-03-19 14:57:38 +0530

    added min heap using array

---
+++ Heap/HeapExample2.java
@@ -0,0 +1,88 @@
+package Heap;
+
+public class HeapExample2 {//MinHeap using array 
+
+
+   static class MinHeap{
+      private int maxSize ;
+      private int size;
+      private int Heap[];
+
+
+      MinHeap(int maxSize){
+         this.size=0;
+         this.maxSize=maxSize;
+         Heap = new int [this.maxSize+1];
+         Heap[0]=Integer.MAX_VALUE;
+
+      }
+
+      public int parent(int pos){
+         return pos/2;
+
+      }
+      public int leftChild(int pos){
+         return 2*pos;
+      }
+      public int rightChild(int pos){
+         return (2*pos)+1;
+      }
+      public boolean iSLeafNode(int pos){
+         if(pos>size/2 && pos<=size){
+            return true;
+         }return false;
+      }
+
+      public void insert(int item){
+         Heap[++size]=item;
+         int current=size;
+
+         while(Heap[current]<Heap[parent(current)]){
+            swap(current,parent(current));
+            current=parent(current);
+         }
+      }
+
+      public void swap(int fpos,int spos){
+         int tmp=Heap[fpos];
+         Heap[fpos]=Heap[spos];
+         Heap[spos]=tmp;
+
+
+      }
+
+      public void deleteRoot(){
+         int tmp=Heap[1];
+         Heap[1]=Heap[size];
+         size--;
+         MinHeapfy(tmp);
+      }
+
+
+      public void MinHeapfy(int pos){
+
+         if(!iSLeafNode(pos)){
+
+            if (Heap[pos]>Heap[leftChild(pos)] || Heap[pos]>Heap[rightChild(pos)]){
+
+               if (Heap[leftChild(pos)]<Heap[rightChild(pos)]){
+
+                  swap(pos,rightChild(pos));
+                  MinHeapfy(rightChild(pos));
+
+               }
+               else{
+                  swap(pos, leftChild(pos));
+                  MinHeapfy(leftChild(pos));
+               }
+            }
+         }
+
+      }
+
+
+
+   
+   }
+   
+}
---
+++ Heap/HeapExample3.java
@@ -0,0 +1,5 @@
+package Heap;
+
+public class HeapExample3 {//Min heap using priorityqueue
+   
+}
